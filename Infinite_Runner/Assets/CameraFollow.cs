d337b765fe915bd5d236e9c4c2ce6c43001aa7bc
TanmayBaravkar <sm1020015@seamedu.in>
2021-07-22 20:46:12 +0530

    Update

--- Infinite_Runner/Assets/CameraFollow.cs
+++ Infinite_Runner/Assets/CameraFollow.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] float Speed = 2f;
+    [SerializeField] GameObject Player;
+    [SerializeField] GameObject Youlose_;
+    [SerializeField] float Offsets;
+
+    private float CurrentPlayer_Dis, CurrentCamera_Dis;
+    
     void Start()
     {
         
@@ -13,6 +19,20 @@ public class CameraFollow : MonoBehaviour
     
     void Update()
     {
+        CurrentPlayer_Dis = Player.transform.position.x;
+        CurrentCamera_Dis = gameObject.transform.position.x;
+
+       CheckPlayerDistance();
        transform.Translate(Vector3.right *Speed *Time.deltaTime);
+    }
+
+    void CheckPlayerDistance()
+    {
+        if (CurrentPlayer_Dis + Offsets < CurrentCamera_Dis)
+        {
+            Destroy(Player);
+            Youlose_.SetActive(true);
+            Time.timeScale = 0;
+        }
     }
 }
